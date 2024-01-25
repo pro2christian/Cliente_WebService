0c8537f98e67af56ba3254bce9e2761c5b6a4711
Christian Oliveira da Silva <pro2christian@icloud.com>
2024-01-24 23:13:56 -0300

    buttons-add

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.EnviaStr_Text = new System.Windows.Forms.TextBox();
+            this.RecebeStr_Text = new System.Windows.Forms.TextBox();
+            this.enviaStr_button = new System.Windows.Forms.Button();
+            this.recebeStr_button = new System.Windows.Forms.Button();
+            this.limpar_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EnviaStr_Text
+            // 
+            this.EnviaStr_Text.Location = new System.Drawing.Point(31, 127);
+            this.EnviaStr_Text.Name = "EnviaStr_Text";
+            this.EnviaStr_Text.Size = new System.Drawing.Size(201, 20);
+            this.EnviaStr_Text.TabIndex = 1;
+            // 
+            // RecebeStr_Text
+            // 
+            this.RecebeStr_Text.Location = new System.Drawing.Point(251, 126);
+            this.RecebeStr_Text.Name = "RecebeStr_Text";
+            this.RecebeStr_Text.Size = new System.Drawing.Size(192, 20);
+            this.RecebeStr_Text.TabIndex = 2;
+            // 
+            // enviaStr_button
+            // 
+            this.enviaStr_button.Location = new System.Drawing.Point(31, 169);
+            this.enviaStr_button.Name = "enviaStr_button";
+            this.enviaStr_button.Size = new System.Drawing.Size(201, 23);
+            this.enviaStr_button.TabIndex = 3;
+            this.enviaStr_button.Text = "Envia String";
+            this.enviaStr_button.UseVisualStyleBackColor = true;
+            this.enviaStr_button.Click += new System.EventHandler(this.enviaStr_Click);
+            // 
+            // recebeStr_button
+            // 
+            this.recebeStr_button.Location = new System.Drawing.Point(251, 169);
+            this.recebeStr_button.Name = "recebeStr_button";
+            this.recebeStr_button.Size = new System.Drawing.Size(192, 23);
+            this.recebeStr_button.TabIndex = 4;
+            this.recebeStr_button.Text = "Recebe String";
+            this.recebeStr_button.UseVisualStyleBackColor = true;
+            this.recebeStr_button.Click += new System.EventHandler(this.recebeStr_Click);
+            // 
+            // limpar_Button
+            // 
+            this.limpar_Button.Location = new System.Drawing.Point(251, 198);
+            this.limpar_Button.Name = "limpar_Button";
+            this.limpar_Button.Size = new System.Drawing.Size(192, 23);
+            this.limpar_Button.TabIndex = 5;
+            this.limpar_Button.Text = "Limpar";
+            this.limpar_Button.UseVisualStyleBackColor = true;
+            this.limpar_Button.Visible = false;
+            this.limpar_Button.Click += new System.EventHandler(this.limpar_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(474, 274);
+            this.Controls.Add(this.limpar_Button);
+            this.Controls.Add(this.recebeStr_button);
+            this.Controls.Add(this.enviaStr_button);
+            this.Controls.Add(this.RecebeStr_Text);
+            this.Controls.Add(this.EnviaStr_Text);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox EnviaStr_Text;
+        private System.Windows.Forms.TextBox RecebeStr_Text;
+        private System.Windows.Forms.Button enviaStr_button;
+        private System.Windows.Forms.Button recebeStr_button;
+        private System.Windows.Forms.Button limpar_Button;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cliente_WebService.ServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +9,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Cliente_WebService
 {
     public partial class Form1 : Form
     {
+        ContratoServidorClient contratoServidorClient;
         public Form1()
         {
             InitializeComponent();
+            EnviaStr_Text.Focus();
+        }
+
+        private void enviaStr_Click(object sender, EventArgs e)
+        {
+            contratoServidorClient = new ContratoServidorClient();
+            string textTemp = EnviaStr_Text.Text;
+            if (string.IsNullOrEmpty(EnviaStr_Text.Text))
+                MessageBox.Show("Nada foi digitado!!","Erro!",
+                    MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            else
+            contratoServidorClient.EnviaStrToServidor(textTemp);
+            contratoServidorClient.Close();
+        }
+
+        private void recebeStr_Click(object sender, EventArgs e)
+        {
+            contratoServidorClient = new ContratoServidorClient();
+            RecebeStr_Text.Text = contratoServidorClient.PegaStrFromServidor();
+            contratoServidorClient.Close();
+            limpar_Button.Visible = true;
+        }
+
+        private void limpar_Button_Click(object sender, EventArgs e)
+        {
+            RecebeStr_Text.Clear();
+            limpar_Button.Visible=false;
         }
     }
 }
